fffeae2bdd34987065c421683eb0f4cff03f094e
Francesco Milano <122035005+F-Milano@users.noreply.github.com>
2026-09-23 18:34:30 +0200

    Update project.js

--- projects/ciabot-montebellina/project.js
+++ projects/ciabot-montebellina/project.js
@@ -1,1 +1,22 @@
+export default {
+    id: "ciabot-montebellina",
+    title: "Ciabot Montebellina",
 
+    icon: "projects/ciabot-montebellina/icon.webp",
+
+    description: `
+        Project description.
+    `,
+
+    year: "",
+    location: "",
+
+    credits: `
+        Project credits.
+    `,
+
+    color: "#711FFF",
+    textColor: "#FF3224",
+
+    images: []
+};
