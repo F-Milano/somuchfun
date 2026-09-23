c62fd1acf691767e4ac8cd551dd6581023612f49
Francesco Milano <122035005+F-Milano@users.noreply.github.com>
2026-09-23 18:35:23 +0200

    Update project.js

--- projects/smart-pasta/project.js
+++ projects/smart-pasta/project.js
@@ -1,1 +1,22 @@
+export default {
+    id: "smart-pasta",
+    title: "Smart Pasta",
 
+    icon: "projects/smart-pasta/icon.webp",
+
+    description: `
+        Project description.
+    `,
+
+    year: "",
+    location: "",
+
+    credits: `
+        Project credits.
+    `,
+
+    color: "#711FFF",
+    textColor: "#FF3224",
+
+    images: []
+};
