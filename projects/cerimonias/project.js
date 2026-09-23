f5ef4289a868d6f57a6dddd592920e3d3fd82e2c
Francesco Milano <122035005+F-Milano@users.noreply.github.com>
2026-09-23 18:34:57 +0200

    Update project.js

--- projects/cerimonias/project.js
+++ projects/cerimonias/project.js
@@ -1,1 +1,22 @@
+export default {
+    id: "cerimonias",
+    title: "Cerimonias",
 
+    icon: "projects/cerimonias/icon.webp",
+
+    description: `
+        Project description.
+    `,
+
+    year: "",
+    location: "",
+
+    credits: `
+        Project credits.
+    `,
+
+    color: "#711FFF",
+    textColor: "#FF3224",
+
+    images: []
+};
